bc2945732e3541326adc8d1a6cc7d85d6b9c1b49
Minh Pham <giaminh2042004@gmail.com>
2025-03-05 14:40:16 +0700

    feat: add LibraryContext for Entity Framework Core integration

---
+++ Data/Library.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleBookLibraryApi.Models;
+
+namespace LibraryApi.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
